7176d24b0d1ab46fa8b03fc6e90b7dfa1a4e81d6
FerdaoussCHAKROUN <71392449+FerdaoussCHAKROUN@users.noreply.github.com>
2021-11-10 02:06:47 +0100

    parser.py

--- parser.py
+++ parser.py
@@ -1,14 +1,14 @@
 from lxml import etree
 
-# pour parser
+# Pour parser
 xml_doc = etree.parse('BigVir2084.xml')
 schema = etree.parse('BigVir2084.xsd')
 
-# pour valider un xml
+# Pour valider un xml
 xml_validator = etree.XMLSchema(schema)
 xml_validator.validate(xml_doc) # true ou false
 
-# pour faire des transformation
+# Pour faire des transformations
 transformateur = etree.XSLT(t)
 output = etree.tostring(transformateur(xml_doc), pretty_print = True)
 ouput = output.decode(encoding = 'utf-8') # pour convertir en string
